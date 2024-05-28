7b8bef54757e1ffc40efa99ba0c71e4e0f42ba37
Zongrui Ma <theoneuma9@gmail.com>
2024-05-28 17:40:24 -0400

    pages function test

--- src/config/http/index.js
+++ src/config/http/index.js
@@ -2,7 +2,7 @@ import request from '@/config/request'
 
 export function sendEmail(data) {
     return request.post(
-        'http://127.0.0.1:8788/api/email/sendEmail',
+        '/api/email/sendEmail',
         {
             data
         }
